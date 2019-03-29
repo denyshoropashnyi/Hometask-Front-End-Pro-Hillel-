acdfc048b6ff50d7165ffd869446f0d09a90072a
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-03-29 16:07:07 +0200

    temporary version.

--- 10 Hometask (Доделать Contact List) - Horopashnyi.Rev01/contacts/app.js
+++ 10 Hometask (Доделать Contact List) - Horopashnyi.Rev01/contacts/app.js
@@ -3,10 +3,31 @@ const contactsList = document.getElementById('contactsList');
 const contactNameInput = document.getElementById('nameInput');
 const contactPhoneInput = document.getElementById('phoneInput');
 const contactAgeInput = document.getElementById('ageInput');
+
+
+const delContactBtn = document.getElementById('delContactBtn');
+
+
 const contactTemplate = document.getElementById('contactTemplate').innerHTML;
 
+
+delContactBtn.addEventListener('click', ondelContactBtnClick)
+
+
 addContactBtn.addEventListener('click', onAddContactBtnClick);
-addContact({ name: 'Alex', phone: '111' });
+// addContact({ name: 'Alex', phone: '111' });
+
+function ondelContactBtnClick() {
+    deleteContact();
+}
+
+function deleteContact() {
+    let deleteBtnTd = document.getElementById('delContactBtn');
+
+    for (let i = 0; i < deleteBtnTd.length; i++) {
+        deleteBtnTd.parentNode.remove();
+    }
+}
 
 function onAddContactBtnClick() {
     submitContact();
@@ -17,6 +38,9 @@ function submitContact() {
         name: contactNameInput.value,
         phone: contactPhoneInput.value,
         age: contactAgeInput.value,
+
+        // delete: delContactBtn.value
+
     }
     addContact(contact);
     resetContactForm();
@@ -28,7 +52,13 @@ function addContact(contact) {
     contactTr.innerHTML = contactTemplate
         .replace('{{name}}', contact.name)
         .replace('{{phone}}', contact.phone)
-        .replace('{{age}}', contact.age || '-');
+        .replace('{{age}}', contact.age || '-')
+
+
+        .replace('{{delete}}', '<button id="delContactBtn">Click me to delete</button>')
+
+
+
 
     contactsList.appendChild(contactTr);
 
