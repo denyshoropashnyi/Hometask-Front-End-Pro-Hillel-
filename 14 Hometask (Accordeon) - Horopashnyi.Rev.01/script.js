dc462a2800b78a4a680fa79757da960d11014bf4
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-04-13 11:24:18 +0300

    Rev.02. - Raw Data

--- 14 Hometask (Accordeon) - Horopashnyi.Rev.01/script.js
+++ 14 Hometask (Accordeon) - Horopashnyi.Rev.01/script.js
@@ -2,15 +2,14 @@
 
 'use strict';
 
+function Accordion(el, config) {
+};
 
-function Accordeon(el, config) {
-}
-
-const accordeon = new Accordeon(
+const accordion = new Accordion(
     document.getElementById('container'),
     { collapseOther: true }
 );
 
-accordeon.open(0);
-accordeon.close(0);
-accordeon.toggle(1);
+accordion.open(0);
+accordion.close(0);
+accordion.toggle(1);
