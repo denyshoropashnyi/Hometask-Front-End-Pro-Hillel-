beee4e204c999095dd968b857ed342386b4924ea
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-04-14 01:02:04 +0300

    Wrong version.

--- 14 Hometask (Accordeon) - Horopashnyi.Rev.01/script.js
+++ 14 Hometask (Accordeon) - Horopashnyi.Rev.01/script.js
@@ -2,14 +2,68 @@
 
 'use strict';
 
-function Accordeon(el, config) {
+let accElement = document.getElementsByClassName('accordeon-element');
+let accHeading = document.getElementsByClassName('accordeon-heading');
+let i;
+
+for (i = 0; i < accHeading.length; i++) {
+    accHeading[i].addEventListener('click', toggleItem)
 }
 
-const accordeon = new Accordeon(
-    document.getElementById('container'),
-    { collapseOther: true }
-);
+function toggleItem() {
+    let elementClass = this.parentNode.className;
+    for (i = 0; i < accElement.length; i++) {
+        accElement[i].className = 'accordeon-show';
+    }
+    if (elementClass == 'accordeon-show') {
+        this.nextElementSibling.className = 'accordeon-hide'
+    }
+};
 
-// accordeon.open(0);
-// accordeon.close(0);
-// accordeon.toggle(1);
+
+
+
+// let accElement = document.getElementsByClassName('accordeon-heading');
+
+// for (let i = 0; i < accElement.length; i++) {
+//     accElement[i].addEventListener('onclick',this.toggle('active'));
+
+// this.classList.toggle('accordeon-body.show');
+
+// }
+
+
+// let accElement = document.getElementsByClassName('accordeon-element');
+
+// let accHeading = document.getElementsByClassName('accordeon-heading');
+
+// let i;
+
+// for (i = 0; i < accHeading.length; i++) {
+//     accHeading[i].addEventListener('click', toggleElement)
+// }
+
+// function toggleElement() {
+//     let elementClass = this.className;
+//     for (i = 0; i < accElement.length; i++) {
+//         accElement[i].className = 'accordeon-body-close';
+//     }
+//     if (elementClass == 'accordeon-body-close') {
+//         this.className == 'accordeon-body-show';
+//     }
+// }
+
+
+
+
+// // function Accordeon(el, config) {
+// // }
+
+// // const accordeon = new Accordeon(
+// //     document.getElementById('container'),
+// //     { collapseOther: true }
+// // );
+
+// // accordeon.open(0);
+// // accordeon.close(0);
+// // accordeon.toggle(1);
