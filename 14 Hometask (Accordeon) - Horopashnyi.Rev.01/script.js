a874f2178645f3553c3d9d3f2d3b04f47085bc4b
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-04-14 11:36:39 +0300

    Not works. Not correct.

--- 14 Hometask (Accordeon) - Horopashnyi.Rev.01/script.js
+++ 14 Hometask (Accordeon) - Horopashnyi.Rev.01/script.js
@@ -8,50 +8,18 @@ let i;
 
 for (i = 0; i < accHeading.length; i++) {
     accHeading[i].addEventListener('click', toggleItem)
-}
+};
 
 function toggleItem() {
     let elementClass = this.parentNode.className;
     for (i = 0; i < accElement.length; i++) {
-        accElement[i].className = 'accordeon-show';
+        this.nextElementSibling.className = 'accordeon-show'
     }
-    if (elementClass == 'accordeon-show') {
-        this.nextElementSibling.className = 'accordeon-hide'
+    if (this.elementClass == 'accordeon-body.active') {
+        this.nextElementSibling.className = 'accordeon-show'
     }
 };
 
-
-
-
-// let accElement = document.getElementsByClassName('accordeon-heading');
-
-// for (let i = 0; i < accElement.length; i++) {
-//     accElement[i].addEventListener('onclick',this.toggle('active'));
-
-// this.classList.toggle('accordeon-body.show');
-
-// }
-
-
-// let accElement = document.getElementsByClassName('accordeon-element');
-
-// let accHeading = document.getElementsByClassName('accordeon-heading');
-
-// let i;
-
-// for (i = 0; i < accHeading.length; i++) {
-//     accHeading[i].addEventListener('click', toggleElement)
-// }
-
-// function toggleElement() {
-//     let elementClass = this.className;
-//     for (i = 0; i < accElement.length; i++) {
-//         accElement[i].className = 'accordeon-body-close';
-//     }
-//     if (elementClass == 'accordeon-body-close') {
-//         this.className == 'accordeon-body-show';
-//     }
-// }
 
 
 
