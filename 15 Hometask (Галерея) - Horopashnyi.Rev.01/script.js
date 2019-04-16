39a2332df1cb703fc3a54b99e591a61afde69d7d
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-04-17 00:21:38 +0300

    In process.

--- 15 Hometask (Галерея) - Horopashnyi.Rev.01/script.js
+++ 15 Hometask (Галерея) - Horopashnyi.Rev.01/script.js
@@ -1,3 +1,58 @@
 //@ts-check
 
 'use strict';
+
+const galleryContainer = document.createElement('div');
+const gallery = document.getElementById('container');
+
+const btnPrevImg = document.createElement('button');
+const btnNextImg = document.createElement('button');
+
+const allImages = gallery.children;
+
+
+galleryContainer.setAttribute('id', 'gallery-container');
+document.body.insertBefore(galleryContainer, document.body.firstChild);
+
+galleryContainer.appendChild(gallery);
+
+btnPrevImg.setAttribute('id', 'btn-prev-img');
+btnPrevImg.innerHTML = ('&#11164;');
+galleryContainer.prepend(btnPrevImg);
+
+btnNextImg.setAttribute('id', 'btn-next-img');
+btnNextImg.innerHTML = ('&#11166;');
+galleryContainer.appendChild(btnNextImg);
+
+
+class Gallery {
+    constructor(el, { delay }) {
+        this.el = el;
+        this.delay = delay;
+
+        this.init();
+    }
+
+    init() {
+
+    }
+
+};
+
+
+btnNextImg.addEventListener('click', showNextImg);
+
+function showNextImg() {
+    for (let i = 0; i < allImages.length; i++) {
+        allImages[i].classList.add('nextImg');
+    };
+    showNextImg()
+};
+
+btnPrevImg.addEventListener('click', showPrevImg);
+
+function showPrevImg() {
+    for (let i = 0; i < allImages.length; i++) {
+        allImages[i].classList.add('prevImg');
+    };
+};
