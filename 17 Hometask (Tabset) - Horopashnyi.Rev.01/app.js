836afea08a5901a688c301a59f9bcb54fca40256
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-04-23 22:13:40 +0300

    Some work.

--- 17 Hometask (Tabset) - Horopashnyi.Rev.01/app.js
+++ 17 Hometask (Tabset) - Horopashnyi.Rev.01/app.js
@@ -6,43 +6,69 @@ class Tabset {
         this.el = el;
 
         this.init();
+
+        this.showCls = 'tabset__element--body--show';
+
+        this.openedElem = 1;
     }
 
     init() {
-        this.addClassName();
+        this.addClassNames();
+        this.addListeners();
     }
 
-    addClassName() {
-        this.wrapperTabset = document.getElementById('container');
-        this.wrapperTabset.classList.add('wrapper__tabset');
+    addClassNames() {
+        this.el.classList.add('wrapper__tabset');
 
-        this.tabsetElement = this.wrapperTabset.children;
+        this.tabsetElement = this.el.children;
         for (let i = 0; i < this.tabsetElement.length; i++) {
             this.tabsetElement[i].classList.add('tabset__element');
         }
 
-        this.tabsetElementHeading = this.wrapperTabset.getElementsByClassName('tabset-heading');
+        this.tabsetElementHeading = this.el.getElementsByClassName('tabset-heading');
         for (let i = 0; i < this.tabsetElementHeading.length; i++) {
             this.tabsetElementHeading[i].classList.add('tabset__element--heading');
         }
 
-        this.tabsetElementBody = this.wrapperTabset.getElementsByClassName('tabset-body');
+        this.tabsetElementBody = this.el.getElementsByClassName('tabset-body');
         for (let i = 0; i < this.tabsetElementBody.length; i++) {
             this.tabsetElementBody[i].classList.add('tabset__element--body');
         };
     }
-    
+
+    addListeners() {
+        for (let item = 0; item < this.tabsetElementHeading.length; item++) {
+            this.tabsetElementHeading[item].addEventListener('click', this.showBody.bind(this) );
+        }
+    }
+
+    showBody(event) {
+        this.removeBody();
+        event.target.nextElementSibling.classList.add(this.showCls);
+        //this.openedElem = item;
+
+    }
+
+    removeBody() {
+        for (let item of this.tabsetElementBody) {
+            item.classList.remove(this.showCls);
+        }
+    }
+
+    show(e) {
+        this.tabsetElementBody[e - 1].classList.add(this.showCls);
+    }
+
+    next() {
+
+    }
 };
-
-
-
-
-
 
 
 const tabs = new Tabset(
     document.getElementById('container')
 );
+
 
 // tabs.show();
 // tabs.next();
