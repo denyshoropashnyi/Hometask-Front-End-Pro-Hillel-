e8f018701bf0858cba4df186767479a9f8b11965
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-05-02 11:23:30 +0300

    Version to review.

--- 18 Hometask (Список пользователей) - Horopashnyi.Rev.01/app.js
+++ 18 Hometask (Список пользователей) - Horopashnyi.Rev.01/app.js
@@ -1,16 +1,32 @@
-// // @ts-check
+// @ts-check
 'use strict';
 
 class Users {
+    constructor(element) {
+        this.element = element;
+        this.init();
+    }
 
+    init() {
+        const basicUrl = 'https://jsonplaceholder.typicode.com/';
+        const userUrl = 'users';
+        request('get', (basicUrl + userUrl), (resp) => { this.renderContacts(resp) });
+    }
+
+    renderContacts(resp) {
+        const userTemplate = document.getElementById('userTemplate');
+        const renderList = userTemplate.innerHTML;
+        let inputContacts = '';
+
+        resp.map((item) => {
+            inputContacts += renderList
+                .replace('{{name}}', item.name)
+                .replace('{{phone}}', item.phone)
+                .replace('{{email}}', item.email)
+        })
+        this.element.children[1].innerHTML = inputContacts;
+    }
 };
-
-const usersList = new Users(
-    document.getElementById('usersListTable')
-);
-const basicUrl = 'https://jsonplaceholder.typicode.com/';
-const userUrl = 'users';
-
 
 
 /****** Function to make request *******
@@ -29,4 +45,7 @@ var request = function () {
     };
 }();
 
-request('get', (basicUrl + userUrl), (resp)=> {console.log(resp)});
+
+const usersList = new Users(
+    document.getElementById('usersListTable')
+);
