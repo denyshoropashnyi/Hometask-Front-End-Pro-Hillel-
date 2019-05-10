a4092b37d3998163d75d1cd08c47daec644ee528
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-05-10 20:27:45 +0300

    Version to review.

--- 20 Hometask (Contact list стероидах) - Horopashnyi.Rev.02/temp.js
+++ 20 Hometask (Contact list стероидах) - Horopashnyi.Rev.02/temp.js
@@ -1,27 +1,25 @@
-// // @ts-check
-'use strict';
-
 const USER_LIST_URL = 'http://fep-app.herokuapp.com/api/contacts';
 
 const USER_LIST_TABLE = document.getElementById('usersListTable');
 const USER_LIST_TABLE_BODY = USER_LIST_TABLE.getElementsByTagName('tbody')[0];
 const USER_ROW_TEMPLATE = document.getElementById('userTemplate').innerHTML;
-const USER_SUBMIT = document.getElementById('submit');
 
 const USER_NAME = document.getElementById('name');
 const USER_SURNAME = document.getElementById('surname');
 const USER_TELEPHONE_NUMBER = document.getElementById('phone');
 const USER_EMAIL = document.getElementById('email');
+const SUBMIT = document.getElementById('submit');
 
 let users = [];
 
 init();
 
+
 function init() {
-    USER_SUBMIT.addEventListener('click', onUserSubmit);
+    SUBMIT.addEventListener('click', onBodyClick)
 
     fetchUsers();
-}
+};
 
 function fetchUsers() {
     return fetch(USER_LIST_URL)
@@ -32,14 +30,15 @@ function fetchUsers() {
 
 function addUsers(data) {
     users = data;
+
     return data;
 }
 
 function renderUsers(data) {
-    console.log(data);
-
+    console.log(data)
     USER_LIST_TABLE_BODY.innerHTML = data.map((user) => {
         return USER_ROW_TEMPLATE
+            .replace('{{id}}', user.id)
             .replace('{{name}}', user.name)
             .replace('{{surname}}', user.surname)
             .replace('{{phone}}', user.phone)
@@ -47,16 +46,15 @@ function renderUsers(data) {
     }).join('\n');
 }
 
-function onUserSubmit(event) {
-    event.preventDefault();
-
-    userSubmit();
+function onBodyClick() {
+    submitNewUser();
 }
 
-function userSubmit() {
+function submitNewUser() {
     const newUser = { name: USER_NAME.value, surname: USER_SURNAME.value, phone: USER_TELEPHONE_NUMBER.value, email: USER_EMAIL.value }
 
-    addUser(newUser).then(fetchUsers);
+    addUser(newUser);
+    fetchUsers();
 
     resetForm();
 }
@@ -72,15 +70,7 @@ function addUser(newUser) {
     });
 }
 
-function onUserClick(event) {
-    console.log(event.target)
-    if (event.target.classList.contains('user-item')) {
-        toggleUserState(event.target)
-            .then(fetchUsers);
-    }
-}
-
 function resetForm() {
-    const addNewUser = document.getElementById('addNewUser');
+    const addNewUser = document.getElementById('forms');
     addNewUser.reset();
 }
