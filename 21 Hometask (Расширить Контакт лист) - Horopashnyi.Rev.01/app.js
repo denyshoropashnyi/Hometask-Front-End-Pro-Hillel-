3e4483b7d3b83b65bf30698c79379a6427772f6a
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-05-14 19:56:20 +0300

    Finished first sprint of this homework.

--- 21 Hometask (Расширить Контакт лист) - Horopashnyi.Rev.01/app.js
+++ 21 Hometask (Расширить Контакт лист) - Horopashnyi.Rev.01/app.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const URL = 'http://fep-app.herokuapp.com/api/contacts';
+
 const newContactForm = document.getElementById('newContactForm');
 const contactsList = document.getElementById('contactsList');
 
@@ -11,6 +12,7 @@ const contactPhoneInput = document.getElementById('phoneInput');
 const contactEmailInput = document.getElementById('emailInput');
 
 const contactTemplate = document.getElementById('contactTemplate').innerHTML;
+const editContactTemplate = document.getElementById('editContactTemplate').innerHTML;
 
 
 let contacts = [];
@@ -61,13 +63,50 @@ function newContactFormSubmit(e) {
 
 
 function onContactsListClick(event) {
-    if (event.target.tagName === 'BUTTON') {
+    if (event.target.classList.contains('btn-edit')) {
+        editContact(event.target.parentNode.parentNode)
+    } else if (event.target.classList.contains('btn-delete')) {
         removeContact(event.target.parentNode.parentNode.dataset.contactId)
             .then(fetchContacts);
+    } else if (event.target.classList.contains('btn-cancel')) {
+        fetchContacts();
+    } else if (event.target.classList.contains('btn-save')) {
+        saveContact(event.target.parentNode.parentNode);
     } else {
         toggleContactState(event.target.parentNode.dataset.contactId)
             .then(fetchContacts);
     }
+}
+
+
+function editContact(el) {
+    let id = el.dataset.contactId;
+
+    let contact = contacts.find((c) => c.id == id);
+    el.innerHTML = editContactTemplate
+        .replace('{{id}}', contact.id)
+        .replace('{{name}}', contact.name)
+        .replace('{{surname}}', contact.surname)
+        .replace('{{phone}}', contact.phone)
+        .replace('{{email}}', contact.email)
+        .replace('{{state}}', contact.is_active)
+}
+
+
+function saveContact(el) {
+    let id = el.dataset.contactId;
+
+    let contact = contacts.find((c) => c.id == id);
+
+    contact.name = el.children[0].children[0].value;
+    contact.surname = el.children[1].children[0].value;
+    contact.phone = el.children[2].children[0].value;
+    contact.email = el.children[3].children[0].value;
+
+    console.log(el.children);
+    updateContact(contact);
+    fetchContacts();
+
 }
 
 
@@ -92,7 +131,7 @@ function updateContact(contact) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(contact)
-    });
+    }).then(fetchContacts);
 }
 
 
@@ -124,10 +163,10 @@ function addContact(contact) {
 
 
 function resetContactForm() {
-    //newContactForm.reset();
-
     contactNameInput.value = '';
     contactSurnameInput.value = '';
     contactPhoneInput.value = '';
     emailInput.value = '';
+
+    //newContactForm.reset();
 }
