22c59a76d1fb937b8879b0b968300feeacc9ded9
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-06-08 15:43:13 +0300

    Ball moving is incorrect;

--- 28 Hometask (Canvas) - Horopashnyi.Rev.01/app.js
+++ 28 Hometask (Canvas) - Horopashnyi.Rev.01/app.js
@@ -9,8 +9,8 @@ const ballColorPicker = document.getElementById('ballColoricker');
 let ballRadius = ballRadiusRange.value;
 let ballColor = ballColorPicker.value;
 
-let x = 50;
-let y = 50;
+let x = 250;
+let y = 250;
 
 
 init();
@@ -18,13 +18,15 @@ init();
 
 function init() {
     // window.addEventListener("load", drawField);
-    onInputChange();
     drawField();
+    onInputChange();
+
+    // onBoardEvent();
     animateXLeft();
     animateXRight();
-    animateY();
+    animateYTop();
+    animateYDown();
     bindEventListeners();
-    // onBoardEvent();
 }
 
 
@@ -32,14 +34,22 @@ function drawField() {
     if (myField.getContext) {
         ctx.fillStyle = "#45E31C";
         ctx.fillRect(0, 0, myField.width, myField.height);
+    } else {
+        alert('canvas-unsupported code here');
+    }
+}
 
+function drawBall() {
+    if (myField.getContext) {
+        ctx.clearRect(0, 0, myField.width, myField.height);
         let circle = new Path2D();
         ctx.beginPath();
-        circle.arc(10, 10, ballRadius, 0, 2 * Math.PI);
+        circle.arc(x, y, ballRadius, 0, 2 * Math.PI);
         ctx.fillStyle = ballColor;
         ctx.fill(circle);
         ctx.strokeStyle = "gold";
         ctx.stroke(circle);
+        ctx.closePath();
     } else {
         alert('canvas-unsupported code here');
     }
@@ -53,80 +63,247 @@ function onInputChange() {
 
 function changeBallRadius() {
     ballRadius = ballRadiusRange.value;
-    drawField();
+    drawBall();
 }
 
 function changeBallColor() {
     ballColor = ballColorPicker.value;
-    drawField();
+    drawBall();
 }
 
 
 function animateXLeft() {
     x -= 10;
-    ctx.clearRect(0, 0, myField.width, myField.height);
-    let circle = new Path2D();
-    ctx.beginPath();
-    circle.arc(x, y, ballRadius, 0, 2 * Math.PI);
-    ctx.fillStyle = ballColor;
-    ctx.fill(circle);
-    ctx.strokeStyle = "gold";
-    ctx.stroke(circle);
-    ctx.closePath();
+    drawBall();
 }
 
 function animateXRight() {
     x += 10;
-    ctx.clearRect(0, 0, myField.width, myField.height);
-    let circle = new Path2D();
-    ctx.save();
-    ctx.beginPath();
-    circle.arc(x, y, ballRadius, 0, 2 * Math.PI);
-    ctx.fillStyle = ballColor;
-    ctx.fill(circle);
-    ctx.strokeStyle = "gold";
-    ctx.stroke(circle);
-    ctx.closePath();
-    ctx.restore();
+    drawBall();
 }
 
-function animateY() {
+function animateYTop() {
+    y -= 10;
+    drawBall();
+}
+
+function animateYDown() {
     y += 10;
-    ctx.clearRect(0, 0, myField.width, myField.height);
-    let circle = new Path2D();
-    ctx.beginPath();
-    circle.arc(x, y, ballRadius, 0, 2 * Math.PI);
-    ctx.fillStyle = ballColor;
-    ctx.fill(circle);
-    ctx.strokeStyle = "gold";
-    ctx.stroke(circle);
-    ctx.closePath();
+    drawBall();
+}
+
+function animate(x, y) {
+    drawBall();
 }
 
 function bindEventListeners() {
-    document.body.addEventListener('keyup', onBoardEvent);
+    document.body.addEventListener('keyup', (e) => onBoardEvent(e));
 }
 
 function onBoardEvent(event) {
-    console.log(event.code);
     switch (event.code) {
         case 'ArrowUp':
-            animateY(y += 10);
-            setInterval(animateY, 1000);
+            // clearInterval(timerID);
+            // animateYTop();
+            // setInterval(animateYTop, 1000);
             break;
         case 'ArrowDown':
-            animateY(y -= 10);
-            setInterval(animateY, 1000);
+            // clearInterval(timerID);
+            // animateYDown();
+            setInterval(animateYDown, 1000);
             break;
         case 'ArrowLeft':
-            animateXLeft();
-            setInterval(animateXLeft, 1000);
+            // clearInterval(timerID);
+            // animateXLeft();
+            // setInterval(animateXLeft, 1000);
             break;
         case 'ArrowRight':
-            animateXRight();
+            clearInterval();
+            // animateXRight();
             setInterval(animateXRight, 1000);
             break;
     }
 }
 
-// setInterval(animateXRight, 100);
+let timerID = setInterval(animateYDown(), 200);
+
+// let timerID = setInterval(animateXRight, 500);
+
+// let timerID = setTimeout(animateXRight {timerID animateXRight, 500}, 500);
+
+
+
+
+
+
+
+
+
+
+
+// 'use strict';
+
+// const myField = document.getElementById('myField');
+// const ctx = myField.getContext('2d');
+
+// const ballRadiusRange = document.getElementById('ballRange');
+// const ballColorPicker = document.getElementById('ballColoricker');
+
+// let ballRadius = ballRadiusRange.value;
+// let ballColor = ballColorPicker.value;
+
+// let x = 5;
+// let y = 100;
+
+
+// init();
+
+
+// function init() {
+//     // window.addEventListener("load", drawField);
+//     onInputChange();
+//     // drawField();
+//     // onBoardEvent();
+//     animateXLeft();
+//     animateXRight();
+//     animateYTop();
+//     animateYDown();
+//     bindEventListeners();
+// }
+
+
+// function drawField() {
+//     if (myField.getContext) {
+//         ctx.fillStyle = "#45E31C";
+//         ctx.fillRect(0, 0, myField.width, myField.height);
+
+//         let circle = new Path2D();
+//         ctx.beginPath();
+//         circle.arc(10, 10, ballRadius, 0, 2 * Math.PI);
+//         ctx.fillStyle = ballColor;
+//         ctx.fill(circle);
+//         ctx.strokeStyle = "gold";
+//         ctx.stroke(circle);
+//     } else {
+//         alert('canvas-unsupported code here');
+//     }
+// }
+
+
+// function onInputChange() {
+//     ballRadiusRange.addEventListener('change', changeBallRadius);
+//     ballColorPicker.addEventListener('change', changeBallColor);
+// }
+
+// function changeBallRadius() {
+//     ballRadius = ballRadiusRange.value;
+//     drawField();
+// }
+
+// function changeBallColor() {
+//     ballColor = ballColorPicker.value;
+//     drawField();
+// }
+
+// function animateXLeft() {
+//     x -= 10;
+//     drawCircle();
+
+//     // ctx.clearRect(0, 0, myField.width, myField.height);
+//     // let circle = new Path2D();
+//     // ctx.beginPath();
+//     // circle.arc(x, y, ballRadius, 0, 2 * Math.PI);
+//     // ctx.fillStyle = ballColor;
+//     // ctx.fill(circle);
+//     // ctx.strokeStyle = "gold";
+//     // ctx.stroke(circle);
+// }
+
+// function animateXRight() {
+//     x += 10;
+//     drawCircle();
+//     // let circle = new Path2D();
+//     // ctx.beginPath();
+//     // circle.arc(x, y, ballRadius, 0, 2 * Math.PI);
+//     // ctx.fillStyle = ballColor;
+//     // ctx.fill(circle);
+//     // ctx.strokeStyle = "gold";
+//     // ctx.stroke(circle);
+//     // ctx.closePath();
+// }
+
+// function drawCircle() {
+//     let circle = new Path2D();
+//     ctx.clearRect(0, 0, myField.width, myField.height);
+
+//     ctx.beginPath();
+//     circle.arc(x, y, ballRadius, 0, 2 * Math.PI);
+//     ctx.fillStyle = ballColor;
+//     ctx.fill(circle);
+//     ctx.strokeStyle = "gold";
+//     ctx.stroke(circle);
+//     ctx.closePath();
+// }
+
+// function animateYTop() {
+//     y -= 10;
+//     drawCircle();
+//     // ctx.clearRect(0, 0, myField.width, myField.height);
+//     // let circle = new Path2D();
+//     // ctx.beginPath();
+//     // circle.arc(10, y, ballRadius, 0, 2 * Math.PI);
+//     // ctx.fillStyle = ballColor;
+//     // ctx.fill(circle);
+//     // ctx.strokeStyle = "gold";
+//     // ctx.stroke(circle);
+//     // ctx.closePath();
+// }
+
+// function animateYDown() {
+//     y += 10;
+//     drawCircle();
+//     // ctx.clearRect(0, 0, myField.width, myField.height);
+//     // let circle = new Path2D();
+//     // ctx.beginPath();
+//     // circle.arc(10, y, ballRadius, 0, 2 * Math.PI);
+//     // ctx.fillStyle = ballColor;
+//     // ctx.fill(circle);
+//     // ctx.strokeStyle = "gold";
+//     // ctx.stroke(circle);
+//     // ctx.closePath();
+// }
+
+// function bindEventListeners() {
+//     document.body.addEventListener('keyup', (e) => onBoardEvent(e));
+// }
+
+// function onBoardEvent(event) {
+//     console.log(event.code);
+//     switch (event.code) {
+//         case 'ArrowUp':
+//             // clearInterval(timerID);
+//             animateYTop();
+//             clearInterval();
+//             setInterval(animateYTop, 1000);
+//             break;
+//         case 'ArrowDown':
+//             // clearInterval(timerID);
+//             animateYDown();
+//             setInterval(animateYDown, 1000);
+//             break;
+//         case 'ArrowLeft':
+//             // clearInterval(timerID);
+//             animateXLeft();
+//             setInterval(animateXLeft, 1000);
+//             break;
+//         case 'ArrowRight':
+//             // clearInterval(timerID);
+//             animateXRight();
+//             setInterval(animateXRight, 1000);
+//             break;
+//     }
+// }
+
+// // let timerID = setInterval(animateXRight, 500);
+
+// // let timerID = setTimeout(animateXRight {timerID animateXRight, 500}, 500);
